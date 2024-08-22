316b005c7f82b5448137bfdf43ff9450d3718b9e
Williams439 <andrewpwilliams1776@gmail.com>
2024-08-21 21:35:02 -0400

    add and list features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ShoppingForm from './Components/ShoppingForm/ShoppingForm';
+import ShoppingList from './Components/ShoppingList/ShoppingList';
+
+export default function App() {
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const API_ROOT = "https://two024-mar-node-vbvm.onrender.com";
+
+  const loadData = () => {
+    fetch(`${API_ROOT}/api/list`)
+     .then(x => x.json())
+     .then(response => {
+        setShoppingList(response);
+     });
+  };
+
+  useEffect(loadData, []);
+
+  const addItem = (item, quantity) => {
+      fetch(`${API_ROOT}/api/list/new`, {
+        method: "POST",
+        body: JSON.stringify({
+          item,
+          quantity
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=utf-8'
+        },
+        mode: 'cors'
+      })
+        .then(x => x.json())
+        .then(loadData);
+  };
+
+    return (
+        <div className="App">
+          <header className="App-header">
+             <h1>Shopping List</h1>
+          </header>
+
+          <main>
+              <ShoppingForm addItem={addItem} />
+              <ShoppingList items={shoppingList}/>
+          </main>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/Components/ShoppingList/ShoppingList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function ShoppingItem(props) {
+    return <li>{props.item} ({props.quantity}) <button>Delete</button></li>
+}
+
+export default function ShoppingList({ items }) {
+    
+    const ItemsJsx = items.map(listItem => 
+        <ShoppingItem 
+            key={listItem.id}
+            id={listItem.id}
+            item={listItem.item}
+            quantity={listItem.quantity}
+        />
+    )
+    
+    return (
+        <ul>{ItemsJsx}</ul>
+    );
+}
